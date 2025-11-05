f92eba7388bce0af39549d3ee83fa545775fc9e9
imemilyhuang <emilyhuang405@gmail.com>
2025-11-04 22:43:19 -0600

    fix spontaneous event bug for expiring correctly

--- sidequests/backend/index.js
+++ sidequests/backend/index.js
@@ -7,7 +7,13 @@ dotenv.config();
 
 const supabase = createClient(
   process.env.SUPABASE_URL,
-  process.env.SUPABASE_SERVICE_ROLE_KEY
+  process.env.SUPABASE_SERVICE_ROLE_KEY,
+  {
+    auth: {
+      autoRefreshToken: false,
+      persistSession: false
+    }
+  }
 );
 
 const app = express();
